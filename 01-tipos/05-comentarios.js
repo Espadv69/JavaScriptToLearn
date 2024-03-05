3c73ee652ffa5e547df6917e1b22f561c9074ca7
espadv69 <aitorjob69@gmail.com>
2024-03-05 21:02:59 +0100

    archivos JS y index

---
+++ 01-tipos/05-comentarios.js
@@ -0,0 +1,12 @@
+// La vida no es como un libro
+/*
+todo lo que escriba aqui sera ignorado
+*/
+/**
+ * este es un comentario
+ * de muchas
+ * lineas
+ */
+let numero = 42; // esto es un numero
+
+console.log(numero);
